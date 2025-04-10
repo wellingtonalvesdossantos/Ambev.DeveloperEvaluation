ad846e254a41f321102adbe0aa43a25db028066d
wellingtonalvesdossantos <wellington_alves_13@hotmail.com>
2025-04-10 15:18:33 -0300

    unit tests

---
+++ tests/Ambev.DeveloperEvaluation.Unit/Domain/Validation/CreateCartItemValidatorTests.cs
@@ -0,0 +1,96 @@
+﻿using Ambev.DeveloperEvaluation.Application.ShoppingCarts.CreateCart;
+using FluentAssertions;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Validation
+{
+    public class CreateCartItemValidatorTests
+    {
+        private readonly CreateCartItemValidator _validator;
+
+        public CreateCartItemValidatorTests()
+        {
+            _validator = new CreateCartItemValidator();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_ProductId_Is_Empty()
+        {
+            // Arrange
+            var command = new CreateCartItemCommand
+            {
+                ProductId = Guid.Empty,
+                Quantity = 1
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result
+                .IsValid
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Quantity_Is_Less_Than_One()
+        {
+            // Arrange
+            var command = new CreateCartItemCommand
+            {
+                ProductId = Guid.NewGuid(),
+                Quantity = 0
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result
+                .IsValid
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Quantity_Is_More_Than_Twenty()
+        {
+            // Arrange
+            var command = new CreateCartItemCommand
+            {
+                ProductId = Guid.NewGuid(),
+                Quantity = 21
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result
+                .IsValid
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Command_Is_Valid()
+        {
+            // Arrange
+            var command = new CreateCartItemCommand
+            {
+                ProductId = Guid.NewGuid(),
+                Quantity = 5
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result
+                .IsValid
+                .Should()
+                .BeTrue();
+        }
+    }
+}
---
+++ tests/Ambev.DeveloperEvaluation.Unit/Domain/Validation/CreateCartValidatorTests.cs
@@ -0,0 +1,90 @@
+﻿using Ambev.DeveloperEvaluation.Application.ShoppingCarts.CreateCart;
+using FluentAssertions;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Validation
+{
+    public class CreateCartValidatorTests
+    {
+        private readonly CreateCartValidator _validator;
+
+        public CreateCartValidatorTests()
+        {
+            _validator = new CreateCartValidator();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Branch_Is_Empty()
+        {
+            // Arrange
+            var command = new CreateCartCommand
+            {
+                Branch = string.Empty,
+                Items = new List<CreateCartItemCommand>
+                {
+                    new CreateCartItemCommand
+                    {
+                        ProductId = Guid.NewGuid(),
+                        Quantity = 1
+                    }
+                }
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result
+                .IsValid
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Items_Is_Empty()
+        {
+            // Arrange
+            var command = new CreateCartCommand
+            {
+                Branch = "branch",
+                Items = []
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result
+                .IsValid
+                .Should()
+                .BeFalse();
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Cart_Is_Valid()
+        {
+            // Arrange
+            var command = new CreateCartCommand
+            {
+                Branch = "branch",
+                Items = new List<CreateCartItemCommand>
+                {
+                    new CreateCartItemCommand
+                    {
+                        ProductId = Guid.NewGuid(),
+                        Quantity = 1
+                    }
+                }
+            };
+
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result
+                .IsValid
+                .Should()
+                .BeTrue();
+        }
+    }
+}
